31a3a644672322b4877274fc154591822a580613
Anaya Langer <kajallanger@gmail.com>
2025-04-10 16:28:01 +0530

    after class

---
+++ Script - Hotstar.js
@@ -0,0 +1,31 @@
+function validate(e) {
+  e.preventDefault();
+
+  const email = document.getElementById("email").value;
+  const pass = document.getElementById("password").value;
+  const age = document.getElementById("age").value;
+  const phone = document.getElementById("phone").value;
+
+  const msgBox = document.getElementById("message");
+
+  let message = "";
+
+  if (email === "") {
+    message = "* Enter Email";
+    msgBox.style.color = "red";
+  } else if (pass === "") {
+    message = "* Passward Min Length - 8 Characters";
+    msgBox.style.color = "red";
+  } else if (age === "") {
+    message = "* Age Requirment - 12 - 50 years";
+    msgBox.style.color = "red";
+  } else if (phone === "") {
+    message = "* Enter Phone Number";
+    msgBox.style.color = "red";
+  } else {
+    message = "* Login Successful";
+    msgBox.style.color = "green";
+  }
+
+  msgBox.innerText = message;
+}
